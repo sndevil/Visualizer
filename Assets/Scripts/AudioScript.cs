4aca6a37d8b6f10dcf097739dfd820c52418ffa9
Mohammadreza Rezaei <rezaiemohammadreza@gmail.com>
2018-10-02 01:53:22 +0200

    3D made

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using B83.MathHelpers;
 
 public class AudioScript : MonoBehaviour {
+    const int SampleRate = 48000;
+    const int BufferSize = 1024;
 
-	public Camera c;
+    public Camera c;
 	AudioClip p, previous;
 
     public GameObject prefab;
@@ -14,42 +16,40 @@ public class AudioScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        float linestart = -3.5f;
-        float linestep = 0.03f;
-        for (int i = 0; i < 512; i++)
-        {
-            prefabsList.Add(Instantiate(prefab,new Vector3(linestart,0),new Quaternion(0,0,0,0)));
-            prefabsList[i].SetActive(true);
-            linestart += linestep;
-            linestep *= 0.997f;
-        }
+        Physics.gravity.Set(0, -9.8f, 0);
         Debug.Log (Microphone.devices[1]);
-        p = Microphone.Start(Microphone.devices[1], false, 1, 48000);
+        p = Microphone.Start(Microphone.devices[1], false, 1, SampleRate);
     }
-	// Update is called once per frame
+    // Update is called once per frame
+
+    int framescape = 0;
 	void Update () {
         //Debug.Log(Microphone.GetPosition(Microphone.devices[1]));
         //if (show)
-        p.GetData(whole, 0);
-		ShowData ();
+        if (framescape++ > 10)
+        {
+            p.GetData(whole, 0);
+            ShowData();
+        }
         if (!Microphone.IsRecording(Microphone.devices[1]))
         {
             p.GetData(whole, 0);
             idx = 0;
-            p = Microphone.Start(Microphone.devices[1], false, 1, 48000);
+            p = Microphone.Start(Microphone.devices[1], false, 1, SampleRate);
         }
     }
 
-	float[] whole = new float[48000];
+	float[] whole = new float[SampleRate];
 	int idx = 0;	
 	Vector3 oldRotate = new Vector3(0,0,0);
+    float freqStep = SampleRate / BufferSize;
 
 	void ShowData () {
-		float[] data = new float[1024];
-		if (idx >= 47000 || idx >= Microphone.GetPosition(Microphone.devices[1]) - 1024)
+		float[] data = new float[BufferSize];
+		if (idx >= SampleRate - BufferSize || idx >= Microphone.GetPosition(Microphone.devices[1]) - BufferSize)
 			return;
-		System.Array.Copy (whole, idx, data, 0, 1024);
-		idx += 1024;
+		System.Array.Copy (whole, idx, data, 0, BufferSize);
+		idx += BufferSize;
         //prefabsList[0].GetComponent<PrefabScript>().ChangeSize(data[20]*20);
         //prefabsList[0].GetComponent<PrefabScript>().ChangeColor(data[20]*20, data[20]*20, data[20]*20);
         //Debug.Log (data[0].ToString());
@@ -57,34 +57,37 @@ public class AudioScript : MonoBehaviour {
         //FFT.CalculateFFT
         Complex[] cx = FFT.Float2Complex(data);
 		FFT.CalculateFFT (cx, false);
-
-		float max = 0;
-		for (int i = 0; i < cx.Length / 2; i++) {
-			var value = Mathf.Sqrt(cx[i].fMagnitude * 200);
+        float linestart = -3.5f;
+        float linestep = 0.03f;
+        float max = 0;
+        float fstep = 1f;
+        float fstart = 0;
+        for (int i = 0; i < cx.Length / 2; i=(int)fstart) {
+            fstart += fstep;
+            fstep *= 1.05f;
+            var value = cx[i].fMagnitude*20f;//Mathf.Sqrt(cx[i].fMagnitude * 200);
 			if (value > max)
 				max = value;
-			var R = value;
-			var G = value;
-			var B = value;
+            GameObject tempObject = Instantiate(prefab, new Vector3(linestart, 0), new Quaternion(0, 90, 0, 0));
+            PrefabScript tempScript = tempObject.GetComponent<PrefabScript>();
+            tempScript.shownObject.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            tempScript.ChangeSize(value*20);
+            tempScript.ChangeColor(value,(int)(i*freqStep));
+            tempObject.SetActive(true);
 
-			Color t = new Color(R,G,B);
-            prefabsList[i].GetComponent<PrefabScript>().ChangeSize(value);
-            prefabsList[i].GetComponent<PrefabScript>().ChangeColor(value);
+            linestart += linestep;
+            //linestep *= 0.997f;
 
-            //Debug.DrawLine (new Vector3(linestart,-linestart,linestart),new Vector3(linestart,-linestart,-linestart),t,1f);
-            //Debug.DrawLine (new Vector3 (linestart, -linestart, -linestart), new Vector3 (-linestart, -linestart, -linestart), t, 1f);
-            //Debug.DrawLine (new Vector3 (-linestart, -linestart, -linestart), new Vector3 (-linestart, -linestart, linestart), t, 1f);
-            //Debug.DrawLine (new Vector3 (-linestart, -linestart, linestart), new Vector3 (linestart, -linestart, linestart), t, 1f);
-		}
-		max = max * max * max / 50f;
-		var currot = c.transform.rotation;
+        }
+		//max = max * max * max / 50f;
+		//var currot = c.transform.rotation;
 		//Debug.Log ("curz:" + currot.z.ToString ());
-		var correction = new Vector3 (0.7f - currot.x, -currot.y, -currot.z);
-		correction /= 100f;
-		Vector3 newRotate = new Vector3 ((Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max);
-		newRotate = (newRotate + oldRotate) / 2f;
-		newRotate = (newRotate + 2 * correction) / 3f;
+		//var correction = new Vector3 (0.7f - currot.x, -currot.y, -currot.z);
+		//correction /= 100f;
+		//Vector3 newRotate = new Vector3 ((Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max);
+		//newRotate = (newRotate + oldRotate) / 2f;
+		//newRotate = (newRotate + 2 * correction) / 3f;
 		//c.transform.Rotate (newRotate);
-		oldRotate = newRotate;
+		//oldRotate = newRotate;
 	}
 }
--- Assets/Scripts/PrefabScript.cs
+++ Assets/Scripts/PrefabScript.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class PrefabScript : MonoBehaviour {
 
     public GameObject shownObject;
-	// Use this for initialization
+    private float speed = 0.05f;
+    // Use this for initialization
+    int thrust = 1;
 	void Start () {
-		
+        //gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1f));
+        //gameObject.GetComponent<Rigidbody>().velocity.Set(0, 0, 0);
+        Destroy(this.gameObject, 5f);
 	}
-	
+
+    public Color CurrentColor;// = new Color(0.5f,0.5f,0.5f,1);
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        gameObject.transform.position = gameObject.transform.position - new Vector3(0, 0, speed);
+        //CurrentColor = shownObject.GetComponent<Renderer>().material.GetColor("_Color");
+        /*CurrentColor.a -= 0.05f;
+        shownObject.GetComponent<Renderer>().material.color = CurrentColor;
+        if (CurrentColor.a <= 0f){
+            Destroy(this.gameObject);
+            //Debug.Log("deleting");
+        }*/
+    }
 
     public void ChangeSize(float size) {
         size = Mathf.Abs(size);
-        shownObject.transform.localScale = new Vector3(1, size, size);
+        shownObject.transform.localScale = new Vector3(1 ,size, 1);
     }
 
     public void ChangeColor(float R, float G, float B, float A = 1f)
@@ -25,15 +37,33 @@ public class PrefabScript : MonoBehaviour {
         shownObject.GetComponent<Renderer>().material.color = new Color(R, G, B, A);
     }
 
-    public void ChangeColor(float Val)
+    public void ChangeColor(float Val, int freq)
     {
-        float scale = Val * Val;
+        /*float scale = Val * Val;
         Val *= 10f;
         int val = (int)(Val * 256 * 256 * 256);
         int B = (int)((val % 256) * scale);
         int G = (int)((val / 256) % 256 * scale);
-        int R = (int)(val / 256 * scale);
-        shownObject.GetComponent<Renderer>().material.color = new Color(1f - (256f / R),1f - (256f / G), 1f - (256f / B));
+        int R = (int)(val / 256 * scale);*/
+        CurrentColor = valueToColor(Val,freq);//new Color(1f - (256f / R), 1f - (256f / G), 1f - (256f / B));
+        shownObject.GetComponent<Renderer>().material.color = CurrentColor;
 
+    }
+
+    private Color valueToColor(float value, int freq) {
+        Color output;
+        float value2 = value * value;
+        float value3 = value * value * value;
+        if (freq > 5000) // Violet
+            output = new Color(value, value3, value);
+        else if (freq > 2000) // Blue
+            output = new Color(value2, value3, value);
+        else if (value > 600) // Green
+            output = new Color(value2, value, value2);
+        else if (value > 200) // Yellow
+            output = new Color(value2, value2, value3);
+        else // Red
+            output = new Color(value, value3,value3);
+        return output;
     }
 }
