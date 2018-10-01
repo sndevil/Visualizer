ff4fd86327c936f35926e79a7d7d0b94a7852ea4
Mohammadreza Rezaei <rezaiemohammadreza@gmail.com>
2018-10-01 17:51:13 +0200

    init commit

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using B83.MathHelpers;
+
+public class AudioScript : MonoBehaviour {
+
+	public Camera c;
+	AudioClip p, previous;
+
+    public GameObject prefab;
+
+    private List<GameObject> prefabsList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        float linestart = -3.5f;
+        float linestep = 0.03f;
+        for (int i = 0; i < 512; i++)
+        {
+            prefabsList.Add(Instantiate(prefab,new Vector3(linestart,0),new Quaternion(0,0,0,0)));
+            prefabsList[i].SetActive(true);
+            linestart += linestep;
+            linestep *= 0.997f;
+        }
+        Debug.Log (Microphone.devices[1]);
+        p = Microphone.Start(Microphone.devices[1], false, 1, 48000);
+    }
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Microphone.GetPosition(Microphone.devices[1]));
+        //if (show)
+        p.GetData(whole, 0);
+		ShowData ();
+        if (!Microphone.IsRecording(Microphone.devices[1]))
+        {
+            p.GetData(whole, 0);
+            idx = 0;
+            p = Microphone.Start(Microphone.devices[1], false, 1, 48000);
+        }
+    }
+
+	float[] whole = new float[48000];
+	int idx = 0;	
+	Vector3 oldRotate = new Vector3(0,0,0);
+
+	void ShowData () {
+		float[] data = new float[1024];
+		if (idx >= 47000 || idx >= Microphone.GetPosition(Microphone.devices[1]) - 1024)
+			return;
+		System.Array.Copy (whole, idx, data, 0, 1024);
+		idx += 1024;
+        //prefabsList[0].GetComponent<PrefabScript>().ChangeSize(data[20]*20);
+        //prefabsList[0].GetComponent<PrefabScript>().ChangeColor(data[20]*20, data[20]*20, data[20]*20);
+        //Debug.Log (data[0].ToString());
+        //Debug.Log (p.samples.ToString ());
+        //FFT.CalculateFFT
+        Complex[] cx = FFT.Float2Complex(data);
+		FFT.CalculateFFT (cx, false);
+
+		float max = 0;
+		for (int i = 0; i < cx.Length / 2; i++) {
+			var value = Mathf.Sqrt(cx[i].fMagnitude * 200);
+			if (value > max)
+				max = value;
+			var R = value;
+			var G = value;
+			var B = value;
+
+			Color t = new Color(R,G,B);
+            prefabsList[i].GetComponent<PrefabScript>().ChangeSize(value);
+            prefabsList[i].GetComponent<PrefabScript>().ChangeColor(value);
+
+            //Debug.DrawLine (new Vector3(linestart,-linestart,linestart),new Vector3(linestart,-linestart,-linestart),t,1f);
+            //Debug.DrawLine (new Vector3 (linestart, -linestart, -linestart), new Vector3 (-linestart, -linestart, -linestart), t, 1f);
+            //Debug.DrawLine (new Vector3 (-linestart, -linestart, -linestart), new Vector3 (-linestart, -linestart, linestart), t, 1f);
+            //Debug.DrawLine (new Vector3 (-linestart, -linestart, linestart), new Vector3 (linestart, -linestart, linestart), t, 1f);
+		}
+		max = max * max * max / 50f;
+		var currot = c.transform.rotation;
+		//Debug.Log ("curz:" + currot.z.ToString ());
+		var correction = new Vector3 (0.7f - currot.x, -currot.y, -currot.z);
+		correction /= 100f;
+		Vector3 newRotate = new Vector3 ((Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max, (Random.value * Random.value - 0.5f) * max);
+		newRotate = (newRotate + oldRotate) / 2f;
+		newRotate = (newRotate + 2 * correction) / 3f;
+		//c.transform.Rotate (newRotate);
+		oldRotate = newRotate;
+	}
+}
---
+++ Assets/Scripts/PrefabScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabScript : MonoBehaviour {
+
+    public GameObject shownObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeSize(float size) {
+        size = Mathf.Abs(size);
+        shownObject.transform.localScale = new Vector3(1, size, size);
+    }
+
+    public void ChangeColor(float R, float G, float B, float A = 1f)
+    {
+        shownObject.GetComponent<Renderer>().material.color = new Color(R, G, B, A);
+    }
+
+    public void ChangeColor(float Val)
+    {
+        float scale = Val * Val;
+        Val *= 10f;
+        int val = (int)(Val * 256 * 256 * 256);
+        int B = (int)((val % 256) * scale);
+        int G = (int)((val / 256) % 256 * scale);
+        int R = (int)(val / 256 * scale);
+        shownObject.GetComponent<Renderer>().material.color = new Color(1f - (256f / R),1f - (256f / G), 1f - (256f / B));
+
+    }
+}
